bbdaadd687d33b1c4b91c5bc683027525e1b6cbf
aditikhandagale2527 <aditi.khandagale2527p@ves.ac.in>
2026-06-14 18:12:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.user import router as user_router
 from routes.jobs import router as jobs_router
+from routes.ai import router as ai_router
+
 
 app = FastAPI(title="Career Connector API")
 
@@ -15,6 +17,8 @@ app.add_middleware(
 
 app.include_router(user_router, prefix="/api/users", tags=["Users"])
 app.include_router(jobs_router, prefix="/api/jobs", tags=["Jobs"])
+app.include_router(ai_router, prefix="/api/ai", tags=["AI"])
+
 
 @app.get("/")
 def root():
